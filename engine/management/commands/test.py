167121b24739c31b2f2d69206ed300eea6e2d527
Milena Araujo <milena.salesaraujo@amadeus.com>
2011-05-25 10:04:40 +0200

    Added a handler to differentiate a code search and a keyword search

--- engine/management/commands/test.py
+++ engine/management/commands/test.py
@@ -14,6 +14,57 @@ class Command(NoArgsCommand):
         add_description()
 
 def add_description():
-#    index = gdb.nodes.indexes.create("description", type="fulltext", provider="lucene")
+    index = gdb.nodes.indexes.create("description", type="fulltext", provider="lucene")
 #    airports = get_node_from_index("types", "type", "airport")
+
+    add_new_refnode("hotel")
+    
+    try:
+        file=open("/home/milena/workspace/TSE/geonames-dump/hotel-test.txt");
+        name = file.readline().strip("\n")
+        en = file.readline().strip("\n")
+        pt = file.readline().strip("\n") 
+        fr = file.readline().strip("\n")
+        
+        
+        node = gdb.node(name=name, description=en, description_pt=pt, description_fr=fr)
+        
+        index_node("keywords", "name", name, node)
+        index_node("keywords", "description", en, node)
+        index_node("keywords", "description", pt, node)
+        index_node("keywords", "description", fr, node)
+        
+        relate_with_node(get_node_from_index("types", "type", "hotel"), node, "IS")
+        
+    except:   
+        print traceback.format_exc() 
+    
+    
+def relate_with_node(ref_node, node, type):
+    ref_node.relationships.create(type, node)    
+    
+    
+def add_new_refnode(name):
+    node = gdb.node(type=name)
+    index_node("types", "type", name, node)
+    
+    
+def index_node(ind, key, value, node):
+
+    index = None
+    try:
+        index = gdb.nodes.indexes.get(ind)
+    except NotFoundError:
+        index = gdb.nodes.indexes.create(ind, type="fulltext", provider="lucene")
+        
+    index.add(key, value, node)
+    
+    
+    
+def get_node_from_index(index, key, value):
+    index = gdb.nodes.indexes.get(index)
+    result = index[key][value]
+    if result:
+        return result[0]
+    return result
     
