b7b022c167df787c1063b2f602a2c441e64617ce
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-09-26 12:52:33 +0200

    [Git] Main branch is trunk, no longer master.

--- sql/makegeodb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-createdb geodb
-createlang -d geodb plpgsql
-psql -dgeodb -f /usr/share/postgresql/8.4/contrib/postgis-1.5/postgis.sql 
-psql -dgeodb -f /usr/share/postgresql/8.4/contrib/postgis-1.5/spatial_ref_sys.sql 
-psql -d geodb -f ./sql/create_tables.sql 
