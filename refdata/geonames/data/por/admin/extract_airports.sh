0edf78efe419eea13e9fa4ef2c08b5d72ba55d13
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2012-02-13 02:58:25 +0100

    [Geonames] Improved the extraction utility for air-related POR (points of reference).

--- refdata/geonames/data/por/admin/extract_airports.sh
+++ refdata/geonames/data/por/admin/extract_airports.sh
@@ -36,8 +36,44 @@ DB_PASSWD="geo"
 # Database Name
 DB_NAME="geo_geonames"
 
+# Snapshot date
+SNAPSHOT_DATE=`date "+%Y%m%d"`
+
 # Extract airport/city information from the Geonames tables (in particular,
 # geoname and alternate_name)
 SQL_FILE="extract_airports.sql"
-echo "Export data from the tables of Geonames:"
-mysql -u ${DB_USER} --password=${DB_PASSWD} -P ${DB_PORT} -h ${DB_HOST} ${DB_NAME} < ${SQL_FILE}
+DUMP_FILE="por_air_iata_${SNAPSHOT_DATE}.csv"
+DUMP_FILE_ICAO_ONLY="por_air_icao_only_${SNAPSHOT_DATE}.csv"
+DUMP_FILE_NO_CODE="por_air_nocode_${SNAPSHOT_DATE}.csv"
+echo
+echo "Exporting airport/airbase/heliport data from the tables of Geonames into '${DUMP_FILE}'."
+echo "That operation may take several minutes..."
+mysql -u ${DB_USER} --password=${DB_PASSWD} -P ${DB_PORT} -h ${DB_HOST} ${DB_NAME} > ${DUMP_FILE} < ${SQL_FILE}
+echo "... Done"
+echo
+
+# Remove the first line (header)
+#sed -i -e "s/^code\(.\+\)//g" ${DUMP_FILE}
+#sed -i -e "/^$/d" ${DUMP_FILE}
+
+# Replace the tab characters by the '^' separator
+sed -i -e 's/\t/\^/g' ${DUMP_FILE}
+
+# 1.1. Extract the rows having no IATA and no ICAO code defined
+grep "^NULL\^NULL\^\(.\+\)" ${DUMP_FILE} > ${DUMP_FILE_NO_CODE}
+
+# 1.2. Remove the rows having no IATA and no ICAO code defined
+sed -i -e "s/^NULL\^NULL\^\(.\+\)//g" ${DUMP_FILE}
+sed -i -e "/^$/d" ${DUMP_FILE}
+
+# 2.1. Extract the rows having only a ICAO code defined
+grep "^NULL\^\(.\+\)" ${DUMP_FILE} > ${DUMP_FILE_ICAO_ONLY}
+
+# 2.2. Remove the rows having only a ICAO code defined
+sed -i -e "s/^NULL\^\(.\+\)//g" ${DUMP_FILE}
+sed -i -e "/^$/d" ${DUMP_FILE}
+
+# Replace the NULL fields by empty fields
+#sed -i -e 's/NULL//g' ${DUMP_FILE}
+#sed -i -e 's/NULL//g' ${DUMP_FILE_NO_CODE}
+
