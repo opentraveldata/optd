ce052f0b70bf3a23109a5701641c4d3ed26c0a9d
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2012-03-18 22:36:58 +0100

    [RefData][ORI] Updated the scripts in order to remove the continent code (aka pricing zone) field.

--- refdata/ORI/fill_table_ori_por.sql
+++ refdata/ORI/fill_table_ori_por.sql
@@ -13,6 +13,6 @@ FIELDS TERMINATED BY '^'
   (iata_code, icao_code, is_geonames, geonameid, name, asciiname,
    alternatenames, latitude, longitude, fclass, fcode, 
    country_code, cc2, admin1, admin2, admin3, admin4, 
-   population, elevation, gtopo30, timezone, moddate,
-   is_airport, is_commercial,
+   population, elevation, gtopo30, timezone, gmt_offset, dst_offset, raw_offset,
+   moddate, is_airport, is_commercial,
    city_code, state_code, region_code, location_type);
