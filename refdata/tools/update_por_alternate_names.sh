bc0f3ba62bd72799e289ba784d77b08d11d00682
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2012-04-02 03:18:37 +0200

    [RefData][Trep] Added scripts to generate the POR data files for the OpenTrep project.

---
+++ refdata/tools/update_por_alternate_names.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+#
+# That script generates, from the ORI-maintained POR data file, the two data
+# files suitable for OpenTrep, namely 'trep_place_details.csv' and
+# 'trep_place_names.csv'. Those files are maintained in the /refdata/trep/admin
+# sub-directory of the OpenTravelData project:
+# http://github.com/opentraveldata/optd.
+#
+# One parameter is optional for this script:
+# - the file-path of the ORI-maintained POR public data file.
+#
+
+##
+# Temporary path
+TMP_DIR="/tmp/por"
+
+##
+# Path of the executable: set it to empty when this is the current directory.
+EXEC_PATH=`dirname $0`
+CURRENT_DIR=`pwd`
+if [ ${CURRENT_DIR} -ef ${EXEC_PATH} ]
+then
+	EXEC_PATH="."
+	TMP_DIR="."
+fi
+EXEC_PATH="${EXEC_PATH}/"
+TMP_DIR="${TMP_DIR}/"
+
+if [ ! -d ${TMP_DIR} -o ! -w ${TMP_DIR} ]
+then
+	\mkdir -p ${TMP_DIR}
+fi
+
+##
+# Input
+ORI_POR_FILENAME=ori_por_public.csv
+SORTED_ORI_POR=sorted_${ORI_POR_FILENAME}
+SORTED_CUT_ORI_POR=cut_sorted_${ORI_POR_FILENAME}
+
+##
+# Targets
+TREP_DETAILS_FILENAME=trep_place_details.csv
+TREP_NAMES_FILENAME=trep_place_names.csv
+
+##
+#
+ORI_POR=../ORI/${ORI_POR_FILENAME}
+TREP_DETAILS_FILE=${TMP_DIR}${TREP_DETAILS_FILENAME}
+TREP_NAMES_FILE=${TMP_DIR}${TREP_NAMES_FILENAME}
+
+#
+if [ "$1" = "-h" -o "$1" = "--help" ];
+then
+	echo
+	echo "From the ORI-maintained POR data file, that script generates (into the '${TMP_DIR}' directory) the two data files suitable for OpenTrep,"
+	echo "namely '${TREP_DETAILS_FILE}' and '${TREP_NAMES_FILE}'."
+	echo
+	echo "Usage: $0 [<ORI-maintained POR public file>]"
+	echo "  - Default name for the ORI-maintained POR public file: '${ORI_POR}'"
+	echo
+	exit -1
+fi
+
+##
+# Data dump file with geographical coordinates
+if [ "$1" != "" ];
+then
+	ORI_POR="$1"
+	ORI_POR_FILENAME=`basename ${ORI_POR}`
+	SORTED_ORI_POR=sorted_${ORI_POR_FILENAME}
+	SORTED_CUT_ORI_POR=cut_sorted_${ORI_POR_FILENAME}
+	if [ "${ORI_POR}" = "${ORI_POR_FILENAME}" ]
+	then
+		ORI_POR="${TMP_DIR}${ORI_POR}"
+	fi
+fi
+SORTED_ORI_POR=${TMP_DIR}${SORTED_ORI_POR}
+SORTED_CUT_ORI_POR=${TMP_DIR}${SORTED_CUT_ORI_POR}
+
+if [ ! -f "${ORI_POR}" ]
+then
+	echo
+	echo "The '${ORI_POR}' file does not exist."
+	echo
+	exit -1
+fi
+
+##
+# First, remove the header (first line)
+ORI_POR_TMP=${TMP_DIR}${ORI_POR_FILENAME}.tmp
+sed -e "s/^iata_code\(.\+\)//g" ${ORI_POR} > ${ORI_POR_TMP}
+sed -i -e "/^$/d" ${ORI_POR_TMP}
+
+
+##
+# The ORI-maintained POR file is sorted according to the IATA code (as is the
+# RFD data dump), just to be sure.
+sort -t'^' -k 1,1 ${ORI_POR_TMP} > ${SORTED_ORI_POR}
+\rm -f ${ORI_POR_TMP}
+
+##
+# Only three columns/fields are kept in that version of the file:
+# the airport/city IATA code and the geographical coordinates (latitude,
+# longitude).
+cut -d'^' -f 1,8,9 ${SORTED_ORI_POR} > ${SORTED_CUT_ORI_POR}
+
+##
+# Preparation step
+echo
+echo "Preparation step"
+echo "----------------"
+echo "The '${SORTED_ORI_POR}' and '${SORTED_CUT_ORI_POR}' files have been derived from '${ORI_POR}'."
+echo
+
+##
+# Generate the file with the names related to the ORI places (POR)
+UPDATER_SCRIPT_DETAILS=${EXEC_PATH}update_por_details.awk
+awk -F'^' -f ${UPDATER_SCRIPT_DETAILS} ${SORTED_ORI_POR} > ${TREP_DETAILS_FILE}
+
+##
+# Generate the file with the names related to the ORI places (POR)
+UPDATER_SCRIPT_NAMES=${EXEC_PATH}update_por_alternate_names.awk
+awk -F'^' -f ${UPDATER_SCRIPT_NAMES} ${SORTED_ORI_POR} > ${TREP_NAMES_FILE}
+
+##
+# Reporting
+echo
+echo "Reporting"
+echo "---------"
+echo "See the '${TREP_DETAILS_FILE}' and '${TREP_NAMES_FILE}' files:"
+echo "less ${TREP_DETAILS_FILE} ${TREP_NAMES_FILE}"
+echo
+
---
+++ refdata/tools/update_por_details.awk
@@ -0,0 +1,42 @@
+##
+#
+# Header
+BEGIN {
+	printf ("iata_code^xapian_docid^icao_code^is_geonames^geonameid")
+	printf ("^latitude^longitude^fclass^fcode^country_code^cc2")
+	printf ("^admin1^admin2^admin3^admin4")
+	printf ("^population^elevation^gtopo30")
+	printf ("^timezone^gmt_offset^dst_offset^raw_offset^moddate")
+	printf ("^is_airport^is_commercial")
+	printf ("^city_code^state_code^region_code^location_type\n")
+}
+
+## M A I N
+{
+	# IATA code
+	iata_code = tolower ($1)
+	printf (iata_code)
+
+	# Xapian Document ID (0)
+	printf ("^0")
+
+	#
+	for (i=2; i<5; i++) {
+		# detail = $i
+		detail = tolower ($i)
+		printf ("^" detail)
+	}
+
+	# Do not dump the names (ascii name, UTF name, alternate names),
+	# which are the fields from $6 to $8
+
+	#
+	for (i=8; i<=NF; i++) {
+		# detail = $i
+		detail = tolower ($i)
+		printf ("^" detail)
+	}
+
+	# End of line
+	printf ("\n")
+}
