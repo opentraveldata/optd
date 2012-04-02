ff00b370dfdfcc7c9b0f26d11604ad5519531ceb
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2012-04-02 18:30:01 +0200

    [RefData] Removed the RFD directory, as it is not maintained in a public way.

--- refdata/RFD/crb_continent.sql
+++
@@ -1,4 +0,0 @@
--- INSERTING into CRB_CONTINENT
-Insert into CRB_CONTINENT (CODE,NAME) values ('ITC1','IATA TC1');
-Insert into CRB_CONTINENT (CODE,NAME) values ('ITC2','IATA TC2');
-Insert into CRB_CONTINENT (CODE,NAME) values ('ITC3','IATA TC3');
