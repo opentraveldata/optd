1b55f4d5fe738a37fec698752b0093072a6f3627
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2012-04-02 18:31:02 +0200

    [RefData] Removed the trep directory, as the OpenTrep project manages itself its own database and related data files.

--- refdata/trep/admin/export_ref_place_details.sql
+++
@@ -1,8 +0,0 @@
---
--- Export data from the ref_place_details table
---
-select concat_ws(',', code, city_code, xapian_docid, is_airport, is_city,
-	   is_main, is_commercial, state_code, country_code, region_code,
-	   continent_code, time_zone_grp, longitude, latitude)
-from ref_place_details;
-
