b3ee698038057ee0741dc1de19676e181a12f8b7
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2012-08-19 00:40:49 +0200

    [RefData][Tools] Completed the Shell/AWK scripts generating the dump_from_geonames.csv data file directly from files rather than from MySQL extractions.

--- refdata/tools/extract_por_with_iata_icao.awk
+++ refdata/tools/extract_por_with_iata_icao.awk
@@ -10,13 +10,18 @@
 # See ../geonames/data/por/admin/aggregateGeonamesPor.sh for more details on
 # the way to derive that file from Geonames original data files.
 #
+# The format of allCountries_w_alt.txt file corresponds to what is expected. So,
+# no further processing has to be done on the format here. However, data is extracted
+# in two files, as seen above, just to keep the compatibility with the MySQL-based
+# generation process. Moreover, all the POR having no IATA code is filtered out.
+# Hence, the remaining of the Shell/AWK scripts, then, can be left untouched.
+#
 # Input format:
 # -------------
 # Sample lines for the allCountries_w_alt.txt file:
-# 6299418^Nice Côte d'Azur International Airport^Nice Cote d'Azur International Airport^Aeroport de Nice Cote d'Azur,Aéroport de Nice Côte d'Azur,Flughafen Nizza,LFMN,NCE,Nice Airport,Nice Cote d'Azur International Airport,Nice Côte d'Azur International Airport,Niza Aeropuerto^43.66272^7.20787^S^AIRP^FR^^B8^06^062^06088^0^3^-9999^Europe/Paris^2012-06-30^NCE^LFMN^^http://en.wikipedia.org/wiki/Nice_C%C3%B4te_d%27Azur_Airport^de^Flughafen Nizza^^en^Nice Côte d'Azur International Airport^^es^Niza Aeropuerto^ps^fr^Aéroport de Nice Côte d'Azur^^en^Nice Airport^s
+# NCE^LFMN^6299418^Nice Côte d'Azur International Airport^Nice Cote d'Azur International Airport^43.66272^7.20787^FR^^S^AIRP^B8^06^062^06088^0^3^-9999^Europe/Paris^^^^^Aeroport de Nice Cote d'Azur,Aéroport de Nice Côte d'Azur,Flughafen Nizza,LFMN,NCE,Nice Airport,Nice Cote d'Azur International Airport,Nice Côte d'Azur International Airport,Niza Aeropuerto^http://en.wikipedia.org/wiki/Nice_C%C3%B4te_d%27Azur_Airport^de^Flughafen Nizza^^en^Nice Côte d'Azur International Airport^^es^Niza Aeropuerto^ps^fr^Aéroport de Nice Côte d'Azur^^en^Nice Airport^s
 #
-# Feature code (field #8 here; see also
-# http://www.geonames.org/export/codes.html):
+# Feature code (field #11 here; see also http://www.geonames.org/export/codes.html):
 # AIRB: Air base; AIRF: Air field; AIRH: Heliport; AIRP: Airport; 
 # AIRQ: Abandoned air field; AIRS: Seaplane landing field
 # RSTN: Railway station
@@ -48,44 +53,137 @@ BEGIN {
 	if (iata_cty_file == "") {
 		iata_cty_file = "/dev/stdout"
 	}
+
+	# Header
+	hdr_line = "iata_code^icao_code^geonameid^name^asciiname^latitude^longitude"
+	hdr_line = hdr_line "^country^cc2^fclass^fcode^admin1^admin2^admin3^admin4"
+	hdr_line = hdr_line "^population^elevation^gtopo30"
+	hdr_line = hdr_line "^timezone^GMT_offset^DST_offset^raw_offset^moddate^alternatenames"
+	hdr_line = hdr_line "^wiki_link^altname_iso^altname_text"
+
+	print (hdr_line) > iata_tvl_file
+	print (hdr_line) > iata_cty_file
 }
 
 
 ##
-# The format of allCountries_w_alt.txt file is very similar to the Geonames
-# pristine allCountry.txt data file.
 #
-/^([0-9]{1,9})\^.*\^([0-9]{4}-[0-9]{2}-[0-9]{2})/ {
+function is_tvl_or_cty (feat_code) {
+	# Calculate the flags corresponding to the type
+	is_city = match (feat_code, "^ADM.*$") + match (feat_code, "^PPL.*$")
+	is_travel = match (feat_code, "^AIRB$") + match (feat_code, "^AIRF$")
+	is_travel += match (feat_code, "^AIRH$") + match (feat_code, "^AIRP$")
+	is_travel += match (feat_code, "^AIRS$") + match (feat_code, "^RSTN$")
+	is_travel += match (feat_code, "^BUSTN$") + match (feat_code, "^BUSTP$")
+}
+
+
+##
+# POR entries having neither a IATA nor a ICAO code (vast majority of the POR).
+# Sample:
+# ^^3022309^Cros-de-Cagnes^Cros-de-Cagnes^43.66405^7.1722^FR^^P^PPL^B8^06^061^06027^0^2^19^Europe/Paris^^^^2012-02-27^Cros-de-Cagnes^^^Cros-de-Cagnes^
+#
+/^\^\^([0-9]{1,9})\^.*\^([0-9]{4}-[0-9]{2}-[0-9]{2})/ {
+	# Feature code
+	fcode = $11
+
+	# Calculate whether it is travel- or city-related
+	is_tvl_or_cty(fcode)
+
+	if (is_travel >= 1 || is_city >= 1) {
+		# Set the IATA and ICAO codes to "NULL". AWK recalculates the whole line ($0).
+		OFS = FS
+		$1 = "NULL"
+		$2 = "NULL"
+
+		# 
+		print ($0) > iata_nocode_file
+	}
+}
+
+##
+# POR entries having only a ICAO code (and no IATA code)
+# Sample:
+# ^BGKS^7730417^Kangersuatsiaq Heliport^Kangersuatsiaq Heliport^72.39667^-55.555^GL^^S^AIRH^03^^^^0^^-9999^America/Godthab^^^^2012-02-26^BGKS,KAQ^http://en.wikipedia.org/wiki/Kangersuatsiaq_Heliport^
+#
+/^\^([A-Z0-9]{4})\^([0-9]{1,9})\^.*\^([0-9]{4}-[0-9]{2}-[0-9]{2})/ {
+	# Feature code
+	fcode = $11
+
+	# Calculate whether it is travel- or city-related
+	is_tvl_or_cty(fcode)
+
+	if (is_travel >= 1 || is_city >= 1) {
+		# Set the IATA code to "NULL". AWK recalculates the whole line ($0).
+		OFS = FS
+		$1 = "NULL"
+
+		print ($0) > iata_icaoonly_file
+	}
+}
+
+##
+# The format of allCountries_w_alt.txt file corresponds to what is expected. So,
+# no processing has to be done here. The data file is split in two, so as to keep
+# compatibility with the MySQL-based generation process.
+#
+# NCE^LFMN^6299418^Nice Côte d'Azur International Airport^Nice Cote d'Azur International Airport^43.66272^7.20787^FR^^S^AIRP^B8^06^062^06088^0^3^-9999^Europe/Paris^^^^^Aeroport de Nice Cote d'Azur,Aéroport de Nice Côte d'Azur,Flughafen Nizza,LFMN,NCE,Nice Airport,Nice Cote d'Azur International Airport,Nice Côte d'Azur International Airport,Niza Aeropuerto^http://en.wikipedia.org/wiki/Nice_C%C3%B4te_d%27Azur_Airport^de^Flughafen Nizza^^en^Nice Côte d'Azur International Airport^^es^Niza Aeropuerto^ps^fr^Aéroport de Nice Côte d'Azur^^en^Nice Airport^s
+/^([A-Z0-9]{3})\^([A-Z0-9]{4}|)\^([0-9]{1,9})\^.*\^([0-9]{4}-[0-9]{2}-[0-9]{2})/ {
 	#
 	por_lines++
 
-	# Feature sub-code
-	feat_subcode = $8
+	# Feature code
+	fcode = $11
 
 	# IATA code
-	iata_code = $20
+	iata_code = $1
 
 	# ICAO code
-	icao_code = $21
+	icao_code = $2
 
 	# FAA code
-	faa_code = $22
+	#faa_code = $xx
+
+	# Geoname ID
+	geoname_id = $3
 
 	# English Wikipedia link
-	wiki_link_code = $23
+	wiki_link_code = $25
 
-	# Calculate the flags corresponding to the type
-	is_city = match ($8, "^ADM.*$") + match ($8, "^PPL.*$")
-	is_travel = match ($8, "^AIRB$") + match ($8, "^AIRF$")
-	is_travel += match ($8, "^AIRH$") + match ($8, "^AIRP$")
-	is_travel += match ($8, "^AIRS$") + match ($8, "^RSTN$")
-	is_travel += match ($8, "^BUSTN$") + match ($8, "^BUSTP$")
+	# Calculate whether it is travel- or city-related
+	is_tvl_or_cty(fcode)
 
-	# Dump the line into the output file
+	# Dump the line into one of the output files
 	if (is_travel >= 1) {
-		print ($0) > iata_tvl_file
+		# Travel-related
+
+		if (icao_code != "") {
+			# Travel-related with a ICAO code (e.g., airports)
+			print ($0) > iata_tvl_file
+
+		} else {
+			# Travel-related with no ICAO code (e.g., some airports, train/bus stations).
+			# Set the ICAO code to "NULL". AWK recalculates the whole line ($0).
+			OFS = FS
+			$2 = "NULL"
+
+			print ($0) > iata_noicao_file
+		}
 	}
+
 	if (is_city >= 1) {
+
+		# Normally, the cities have no ICAO code
+		if (icao_code == "") {
+			# Set the ICAO code to "NULL". AWK recalculates the whole line ($0).
+			OFS = FS
+			$2 = "NULL"
+
+		} else {
+			print ("[" FNR "] The POR having got geoname_id=" geoname_id " is a city, but has got a ICAO code (" icao_code "), which is not normal") > "/dev/stderr"
+		}
+		
+		# Cities
 		print ($0) > iata_cty_file
 	}
 }
--- refdata/tools/extract_por_with_iata_icao.sh
+++ refdata/tools/extract_por_with_iata_icao.sh
@@ -64,6 +64,7 @@ then
 	echo
 	echo "Usage: $0"
 	echo "  - Snapshot date: '${SNAPSHOT_DATE}' (${SNAPSHOT_DATE_HUMAN})"
+	echo "  - Input (CSV-formatted) data file: '${GEO_POR_FILE}'"
 	echo "  - Generated (CSV-formatted) data files:"
 	echo "      + '${DUMP_FILE_IATA_TVL}'"
 	echo "      + '${DUMP_FILE_IATA_CTY}'"
@@ -79,10 +80,11 @@ fi
 # 0. Data extraction from the Geonames data file, for travel-related POR
 #    and cities.
 echo
-echo "Exporting points of reference (POR, i.e., airports, railway stations) and populated place (city) data from the tables of Geonames into '${DUMP_FILE_IATA_TVL}' and '${DUMP_FILE_IATA_CTY}' respectively."
+echo "Extracting travel-related points of reference (POR, i.e., airports, railway stations) and populated place (city) data from the Geonames dump data file."
+echo "The '${GEO_POR_FILE}' input data file allows to generate both '${DUMP_FILE_IATA_TVL}' and '${DUMP_FILE_IATA_CTY}' files."
 echo "That operation may take several minutes..."
 IATA_EXTRACTOR=${EXEC_PATH}extract_por_with_iata_icao.awk
-time awk -F'^' -v iata_tvl_file=${DUMP_FILE_IATA_TVL} -v iata_cty_file=${DUMP_FILE_IATA_CTY} -f ${IATA_EXTRACTOR} ${GEO_POR_FILE}
+time awk -F'^' -v iata_tvl_file=${DUMP_FILE_IATA_TVL} -v iata_cty_file=${DUMP_FILE_IATA_CTY} -v iata_icaoonly_file=${DUMP_FILE_ICAO_ONLY} -v iata_nocode_file=${DUMP_FILE_NO_CODE} -v iata_noicao_file=${DUMP_FILE_NO_ICAO} -f ${IATA_EXTRACTOR} ${GEO_POR_FILE}
 echo "... Done"
 echo
 
@@ -92,74 +94,39 @@ echo
 #sed -i -e "s/^iata_code\(.\+\)//g" ${DUMP_FILE_IATA_TVL}
 #sed -i -e "/^$/d" ${DUMP_FILE_IATA_TVL}
 
-# Replace the tab characters by the '^' separator
-sed -i -e 's/\t/\^/g' ${DUMP_FILE_IATA_TVL}
-sed -i -e 's/\t/\^/g' ${DUMP_FILE_IATA_CTY}
-
-# 1.1. Extract the rows having no IATA and no ICAO code defined.
-#      By construction, cities have a IATA code specified.
-grep "^NULL\^NULL\^\(.\+\)" ${DUMP_FILE_IATA_TVL} > ${DUMP_FILE_NO_CODE}
-
-# 1.2. Remove the rows having no IATA and no ICAO code defined
-#      By construction, cities have a IATA code specified.
-sed -i -e "s/^NULL\^NULL\^\(.\+\)//g" ${DUMP_FILE_IATA_TVL}
-sed -i -e "/^$/d" ${DUMP_FILE_IATA_TVL}
-
-# 2.1. Extract the rows having only a ICAO code defined
-#      By construction, cities have no ICAO code specified.
-grep "^NULL\^\(.\+\)" ${DUMP_FILE_IATA_TVL} > ${DUMP_FILE_ICAO_ONLY}
-
-# 2.2. Remove the rows having only a ICAO code defined
-#      By construction, cities have no ICAO code specified.
-sed -i -e "s/^NULL\^\(.\+\)//g" ${DUMP_FILE_IATA_TVL}
-sed -i -e "/^$/d" ${DUMP_FILE_IATA_TVL}
-
 ##
 # We are now left with only the points of interest containing a non-NULL IATA
 # code.
 
-# 3.1. Extract the headers into temporary files
-# 3.1.1. For travel-related POR
+# 1.1. Extract the headers into temporary files
+# 1.1.1. For travel-related POR
 DUMP_FILE_TVL_HDR=${DUMP_FILE_IATA_TVL}.tmp.tvlhdr
 grep "^iata\(.\+\)" ${DUMP_FILE_IATA_TVL} > ${DUMP_FILE_TVL_HDR}
-# 3.1.2. For cities
+# 1.1.2. For cities
 DUMP_FILE_CTY_HDR=${DUMP_FILE_IATA_CTY}.tmp.ctyhdr
 grep "^iata\(.\+\)" ${DUMP_FILE_IATA_CTY} > ${DUMP_FILE_CTY_HDR}
-sed -i -e "s/NULL/icao/g" ${DUMP_FILE_CTY_HDR}
 
-# 3.2. Remove the headers
-# 3.2.1. For travel-related POR
+# 1.2. Remove the headers
+# 1.2.1. For travel-related POR
 sed -i -e "s/^iata\(.\+\)//g" ${DUMP_FILE_IATA_TVL}
 sed -i -e "/^$/d" ${DUMP_FILE_IATA_TVL}
-# 3.2.2. For cities
+# 1.2.2. For cities
 sed -i -e "s/^iata\(.\+\)//g" ${DUMP_FILE_IATA_CTY}
 sed -i -e "/^$/d" ${DUMP_FILE_IATA_CTY}
 
-# 4. Handle ICAO codes for the travel-related POR only. As the cities have no
-#    ICAO code, they are not handled here.
-# 4.1. Extract the entries having no ICAO code.
-grep "^\([A-Z0-9][A-Z0-9][A-Z0-9]\)\^NULL\^\(.\+\)" ${DUMP_FILE_IATA_TVL} > ${DUMP_FILE_NO_ICAO}
+# 2. Sort the files
+DUMP_FILE_TMP=${TMP_DIR}sorted.csv.tmp
+# 2.1. Travel-related
+sort -t '^' -k1,2 ${DUMP_FILE_IATA_TVL} > ${DUMP_FILE_TMP}
+\mv -f ${DUMP_FILE_TMP} ${DUMP_FILE_IATA_TVL}
+# 2.2. Cities
+sort -t '^' -k1,2 ${DUMP_FILE_IATA_CTY} > ${DUMP_FILE_TMP}
+\mv -f ${DUMP_FILE_TMP} ${DUMP_FILE_IATA_CTY}
+# 2.2. No ICAO codes
+sort -t '^' -k1,2 ${DUMP_FILE_NO_ICAO} > ${DUMP_FILE_TMP}
+\mv -f ${DUMP_FILE_TMP} ${DUMP_FILE_NO_ICAO}
 
-# 4.2. Remove the travel-related POR entries having no ICAO code.
-sed -i -e "s/^\([A-Z0-9][A-Z0-9][A-Z0-9]\)\^NULL\^\(.\+\)//g" ${DUMP_FILE_IATA_TVL}
-sed -i -e "/^$/d" ${DUMP_FILE_IATA_TVL}
-
-# 4.3. Aggregate the language (e.g., 'en') alternate names on a single line.
-#      The alternate names of the same POR, specified as having the same IATA
-#      and ICAO codes, as well as the same Geonames ID.
-ALT_NAME_AGGREGATOR=${EXEC_PATH}alt_name_aggregator.awk
-# 4.3.1. For the travel-related POR
-AGG_DUMP_FILE_TMP=${DUMP_FILE_IATA_TVL}.tmp.agg
-awk -F'^' -f ${ALT_NAME_AGGREGATOR} ${DUMP_FILE_IATA_TVL} > ${AGG_DUMP_FILE_TMP}
-\cp -f ${AGG_DUMP_FILE_TMP} ${DUMP_FILE_IATA_TVL}
-# 4.3.2. For the cities
-awk -F'^' -f ${ALT_NAME_AGGREGATOR} ${DUMP_FILE_IATA_CTY} > ${AGG_DUMP_FILE_TMP}
-\cp -f ${AGG_DUMP_FILE_TMP} ${DUMP_FILE_IATA_CTY}
-# 4.3.3. For the travel-related POR having no ICAO code
-awk -F'^' -f ${ALT_NAME_AGGREGATOR} ${DUMP_FILE_NO_ICAO} > ${AGG_DUMP_FILE_TMP}
-\cp -f ${AGG_DUMP_FILE_TMP} ${DUMP_FILE_NO_ICAO}
-
-# 4.4. Spot the (potential) remaining entries having duplicated IATA codes.
+# 3.1. Spot the (potential) remaining entries having duplicated IATA codes.
 #      Here, only the airport entries having duplicated IATA codes are spotted.
 #      That case may typically appear when someone, in Geonames, has mistakenly
 #      set the IATA code (say ACQ; that airport is Waseca Municpal Airport and
@@ -171,14 +138,11 @@ awk -F'^' -f ${ALT_NAME_AGGREGATOR} ${DUMP_FILE_NO_ICAO} > ${AGG_DUMP_FILE_TMP}
 #      compared with the de-duplicated file: the differences are the duplicated
 #      entries.
 #
-# 4.4.1. Create the file with no duplicated IATA code.
+# 3.1.1. Create the file with no duplicated IATA code.
 DUMP_UNIQ_FILE=${DUMP_FILE_IATA_TVL}.tmp.uniq
-DUMP_FILE_TMP=${DUMP_FILE_IATA_TVL}.tmp
-sort -t '^' -k1,2 ${DUMP_FILE_IATA_TVL} > ${DUMP_FILE_TMP}
-\mv -f ${DUMP_FILE_TMP} ${DUMP_FILE_IATA_TVL}
 uniq -w 3 ${DUMP_FILE_IATA_TVL} > ${DUMP_UNIQ_FILE}
 
-# 4.4.2. Create the file with only the duplicated IATA code entries, if any.
+# 3.1.2. Create the file with only the duplicated IATA code entries, if any.
 DUMP_FILE_DUP_CHECK=${DUMP_FILE_DUP}.tmp.check
 comm -23 ${DUMP_FILE_IATA_TVL} ${DUMP_UNIQ_FILE} > ${DUMP_FILE_DUP_CHECK}
 sed -i -e "/^$/d" ${DUMP_FILE_DUP_CHECK}
@@ -198,15 +162,12 @@ else
 	\rm -f ${DUMP_FILE_DUP_CHECK}
 fi
 
-# 4.5. Merge the data files for both POR types (travel-related and cities)
+# 3.2. Merge the data files for both POR types (travel-related and cities)
 cat ${DUMP_UNIQ_FILE} ${DUMP_FILE_IATA_CTY} > ${DUMP_FILE_IATA_ALL}
 sort -t'^' -k1,2 ${DUMP_FILE_IATA_ALL} > ${DUMP_FILE_TMP}
 \mv -f ${DUMP_FILE_TMP} ${DUMP_FILE_IATA_ALL}
 
-# 4.6. Add the Wiki link in the header
-sed -i -e "s/alternatenames\^altname_iso/alternatenames\^wiki_link\^altname_iso/g" ${DUMP_FILE_TVL_HDR}
-
-# 4.7. Re-add the header
+# 3.3. Re-add the header
 cat ${DUMP_FILE_TVL_HDR} ${DUMP_FILE_IATA_ALL} > ${DUMP_FILE_TMP}
 sed -e "/^$/d" ${DUMP_FILE_TMP} > ${DUMP_FILE_IATA_ALL}
 
@@ -217,9 +178,4 @@ then
 	\rm -f ${DUMP_FILE_TVL_HDR} ${DUMP_FILE_TMP} ${DUMP_FILE_CTY_HDR}
 	\rm -f ${DUMP_UNIQ_FILE} ${DUMP_FILE_IATA_CTY_TMP}
 fi
-
-
-# Replace the NULL fields by empty fields
-#sed -i -e 's/NULL//g' ${DUMP_FILE_IATA_TVL}
-#sed -i -e 's/NULL//g' ${DUMP_FILE_NO_CODE}
 
