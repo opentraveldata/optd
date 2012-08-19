17509164dc1e337935c22b1e937709ad9e25f28d
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2012-08-19 12:41:51 +0200

    [RefData][Tools] Made the Geonames data file preparation processing a little bit more robust (regarding the handling of directories).

--- refdata/tools/preprepare_geonames_dump_file.sh
+++ refdata/tools/preprepare_geonames_dump_file.sh
@@ -49,29 +49,57 @@ TMP_DIR="/tmp/por"
 ##
 # Path of the executable: set it to empty when this is the current directory.
 EXEC_PATH=`dirname $0`
+# Trick to get the actual full-path
+EXEC_FULL_PATH=`pushd ${EXEC_PATH}`
+EXEC_FULL_PATH=`echo ${EXEC_FULL_PATH} | cut -d' ' -f1`
+EXEC_FULL_PATH=`echo ${EXEC_FULL_PATH} | sed -e 's|~|'${HOME}'|'`
+#
 CURRENT_DIR=`pwd`
 if [ ${CURRENT_DIR} -ef ${EXEC_PATH} ]
 then
-	EXEC_PATH="./"
-	TMP_DIR="./"
-else
-	EXEC_PATH="${EXEC_PATH}/"
-	TMP_DIR="${TMP_DIR}/"
+	EXEC_PATH="."
+	TMP_DIR="."
 fi
-#
+# If the Geonames dump file is in the current directory, then the current
+# directory is certainly intended to be the temporary directory.
+if [ -f ${GEO_RAW_FILENAME} ]
+then
+	TMP_DIR="."
+fi
+EXEC_PATH="${EXEC_PATH}/"
+TMP_DIR="${TMP_DIR}/"
+
 if [ ! -d ${TMP_DIR} -o ! -w ${TMP_DIR} ]
 then
 	\mkdir -p ${TMP_DIR}
 fi
 
 ##
+# Sanity check: that (executable) script should be located in the tools/ sub-directory
+# of the OpenTravelData project Git clone
+EXEC_DIR_NAME=`basename ${EXEC_FULL_PATH}`
+if [ "${EXEC_DIR_NAME}" != "tools" ]
+then
+	echo
+	echo "[$0:$LINENO] Inconsistency error: this script ($0) should be located in the refdata/tools/ sub-directory of the OpenTravelData project Git clone, but apparently is not. EXEC_FULL_PATH=\"${EXEC_FULL_PATH}\""
+	echo
+	exit -1
+fi
+
+##
+# OpenTravelData directory
+OPTD_DIR=`dirname ${EXEC_FULL_PATH}`
+OPTD_DIR="${OPTD_DIR}/"
+
+##
+# ORI sub-directory
+ORI_DIR=${OPTD_DIR}ORI/
+TOOLS_DIR=${OPTD_DIR}tools/
+
+##
 # Snapshot date
 SNAPSHOT_DATE=`date "+%Y%m%d"`
 SNAPSHOT_DATE_HUMAN=`date`
-
-##
-# ORI directory
-ORI_DIR=../ORI/
 
 ##
 # Input files
@@ -149,7 +177,7 @@ sed -i -e "/^$/d" ${GEO_RAW_FILE}
 
 ##
 # 3.1. Replace the 'NULL' fields by 'ZZZZ', so as to place them at the end
-sed -i -e "s/^\([A-Z0-9][A-Z0-9][A-Z0-9]\)\^NULL\^\(.\+\)/\1\^ZZZZ\^\2/g" ${GEO_RAW_FILE}
+sed -i -e "s/^\([A-Z0-9]\{3\}\)\^NULL\^\(.\+\)/\1\^ZZZZ\^\2/g" ${GEO_RAW_FILE}
 
 # 3.2. Sort the Geonames dump file according to the (IATA, ICAO) code pair
 GEO_RAW_FILE_TMP=${GEO_RAW_FILE}.tmp
