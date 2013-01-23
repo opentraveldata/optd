13dfb06a041559b63e631618dc18d09d5adb403f
dchoe <dongkyoung.choe@amadeus.com>
2013-01-23 18:13:19 +0100

    [ori_airlines] Updated alliance data

--- refdata/ORI/airline_alliance/add_alliance_info.sh
+++ refdata/ORI/airline_alliance/add_alliance_info.sh
@@ -6,7 +6,7 @@ SKYTEAM="S"
 
 BASEDIR=$(dirname $0)
 AIRLINE_CD_CSV=${BASEDIR}"/../ori_airlines.csv"
-AIRLINE_ALLIANCE_CSV="./airline_alliance_121221.csv"
+AIRLINE_ALLIANCE_CSV="./airline_alliance_130123.csv"
 
 OUTPUT_FILE="ori_airlines.csv"
 
@@ -19,9 +19,9 @@ BEGIN{
      }
 
 {
-   if (NR==1) {print $0,"alliance_status"}
-   else if ($3 in alliance) {print $0""alliance[$3]}  
-   else {print $0"^"alliance[$3]}
+   if (NR==1) {print $0}
+   else if ($3 in alliance) {print $1,$2,$3,$4,$5,$6,alliance[$3]}  
+   else {print $1,$2,$3,$4,$5,$6"^^"}
 }' ${AIRLINE_CD_CSV} > ${OUTPUT_FILE}
 
 
